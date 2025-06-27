232fb342b6c3c945795ed168aa8b399ed459cd29
fulleni <fulleni@hotmail.com>
2025-06-27 09:18:22 +0100

    refactor: Simplify main.dart and remove DevicePreview

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,9 @@
-import 'dart:js_interop';
-
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:ht_dashboard/app/config/config.dart';
 import 'package:ht_dashboard/bootstrap.dart';
 
 // Define the current application environment (production/development/demo).
 const AppEnvironment appEnvironment = AppEnvironment.demo;
-
-@JS('removeSplashFromWeb')
-external void removeSplashFromWeb();
 
 void main() async {
   final appConfig = switch (appEnvironment) {
@@ -21,20 +14,5 @@ void main() async {
 
   final appWidget = await bootstrap(appConfig, appEnvironment);
 
-  // Only remove the splash screen on web after the app is ready.
-  if (kIsWeb) {
-    removeSplashFromWeb();
-  }
-
-  if (appConfig.environment == AppEnvironment.demo) {
-    runApp(
-      DevicePreview(
-        builder: (context) => appWidget,
-        tools: const [DeviceSection()],
-        backgroundColor: Colors.black87,
-      ),
-    );
-  } else {
-    runApp(appWidget);
-  }
+  runApp(appWidget);
 }
