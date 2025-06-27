0e77aafb99cfd5ab588c2aa95663f72a3c8d2033
fulleni <fulleni@hotmail.com>
2025-06-27 09:33:50 +0100

    feat: Remove splash screen on web after load
    - Removes splash on web after app loads
    - Improves user experience

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'dart:js_interop';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:ht_dashboard/app/config/config.dart';
 import 'package:ht_dashboard/bootstrap.dart';
 
 // Define the current application environment (production/development/demo).
 const AppEnvironment appEnvironment = AppEnvironment.demo;
+
+@JS('removeSplashFromWeb')
+external void removeSplashFromWeb();
 
 void main() async {
   final appConfig = switch (appEnvironment) {
@@ -14,5 +20,9 @@ void main() async {
 
   final appWidget = await bootstrap(appConfig, appEnvironment);
 
+  // Only remove the splash screen on web after the app is ready.
+  if (kIsWeb) {
+    removeSplashFromWeb();
+  }
   runApp(appWidget);
 }
