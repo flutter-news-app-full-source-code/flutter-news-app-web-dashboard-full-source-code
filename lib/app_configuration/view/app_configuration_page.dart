b31405a89d7fb162576211d0cae80718f09c23b3
fulleni <fulleni@hotmail.com>
2025-06-27 09:46:16 +0100

    feat: add app configuration page
    - Created basic UI
    - Added placeholder text

---
+++ lib/app_configuration/view/app_configuration_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+/// {@template app_configuration_page}
+/// A placeholder page for App Configuration.
+/// {@endtemplate}
+class AppConfigurationPage extends StatelessWidget {
+  /// {@macro app_configuration_page}
+  const AppConfigurationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('App Configuration Page'),
+      ),
+    );
+  }
+}
