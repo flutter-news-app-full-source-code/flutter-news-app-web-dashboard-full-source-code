5a7aefeece40a747ce5a37d9f76c36d2e8647c30
fulleni <fulleni@hotmail.com>
2025-07-01 21:35:14 +0100

    feat(edit_source): add EditSourceEvent
    - Added events for data changes
    - Added event for form submission

---
+++ lib/content_management/bloc/edit_source/edit_source_event.dart
@@ -0,0 +1,79 @@
+part of 'edit_source_bloc.dart';
+
+/// Base class for all events related to the [EditSourceBloc].
+sealed class EditSourceEvent extends Equatable {
+  const EditSourceEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+/// Event to load the initial source data for editing.
+final class EditSourceLoaded extends EditSourceEvent {
+  const EditSourceLoaded();
+}
+
+/// Event triggered when the source name input changes.
+final class EditSourceNameChanged extends EditSourceEvent {
+  const EditSourceNameChanged(this.name);
+
+  final String name;
+
+  @override
+  List<Object> get props => [name];
+}
+
+/// Event triggered when the source description input changes.
+final class EditSourceDescriptionChanged extends EditSourceEvent {
+  const EditSourceDescriptionChanged(this.description);
+
+  final String description;
+
+  @override
+  List<Object> get props => [description];
+}
+
+/// Event triggered when the source URL input changes.
+final class EditSourceUrlChanged extends EditSourceEvent {
+  const EditSourceUrlChanged(this.url);
+
+  final String url;
+
+  @override
+  List<Object> get props => [url];
+}
+
+/// Event triggered when the source type input changes.
+final class EditSourceTypeChanged extends EditSourceEvent {
+  const EditSourceTypeChanged(this.sourceType);
+
+  final SourceType? sourceType;
+
+  @override
+  List<Object?> get props => [sourceType];
+}
+
+/// Event triggered when the source language input changes.
+final class EditSourceLanguageChanged extends EditSourceEvent {
+  const EditSourceLanguageChanged(this.language);
+
+  final String language;
+
+  @override
+  List<Object> get props => [language];
+}
+
+/// Event triggered when the source headquarters input changes.
+final class EditSourceHeadquartersChanged extends EditSourceEvent {
+  const EditSourceHeadquartersChanged(this.headquarters);
+
+  final Country? headquarters;
+
+  @override
+  List<Object?> get props => [headquarters];
+}
+
+/// Event to submit the edited source data.
+final class EditSourceSubmitted extends EditSourceEvent {
+  const EditSourceSubmitted();
+}
