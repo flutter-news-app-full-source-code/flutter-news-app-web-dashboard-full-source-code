6caa1d2085f04d7184e7311c330bec23ece07d14
fulleni <fulleni@hotmail.com>
2025-07-01 22:06:30 +0100

    feat(content): add EditHeadlineEvent
    - Created EditHeadlineEvent class
    - Added events for data changes
    - Added submit event

---
+++ lib/content_management/bloc/edit_headline/edit_headline_event.dart
@@ -0,0 +1,68 @@
+part of 'edit_headline_bloc.dart';
+
+/// Base class for all events related to the [EditHeadlineBloc].
+abstract class EditHeadlineEvent extends Equatable {
+  const EditHeadlineEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+/// Event to signal that the headline data should be loaded.
+class EditHeadlineLoaded extends EditHeadlineEvent {
+  const EditHeadlineLoaded();
+}
+
+/// Event for when the headline's title is changed.
+class EditHeadlineTitleChanged extends EditHeadlineEvent {
+  const EditHeadlineTitleChanged(this.title);
+  final String title;
+  @override
+  List<Object> get props => [title];
+}
+
+/// Event for when the headline's description is changed.
+class EditHeadlineDescriptionChanged extends EditHeadlineEvent {
+  const EditHeadlineDescriptionChanged(this.description);
+  final String description;
+  @override
+  List<Object> get props => [description];
+}
+
+/// Event for when the headline's URL is changed.
+class EditHeadlineUrlChanged extends EditHeadlineEvent {
+  const EditHeadlineUrlChanged(this.url);
+  final String url;
+  @override
+  List<Object> get props => [url];
+}
+
+/// Event for when the headline's image URL is changed.
+class EditHeadlineImageUrlChanged extends EditHeadlineEvent {
+  const EditHeadlineImageUrlChanged(this.imageUrl);
+  final String imageUrl;
+  @override
+  List<Object> get props => [imageUrl];
+}
+
+/// Event for when the headline's source is changed.
+class EditHeadlineSourceChanged extends EditHeadlineEvent {
+  const EditHeadlineSourceChanged(this.source);
+  final Source? source;
+  @override
+  List<Object?> get props => [source];
+}
+
+/// Event for when the headline's category is changed.
+class EditHeadlineCategoryChanged extends EditHeadlineEvent {
+  const EditHeadlineCategoryChanged(this.category);
+  final Category? category;
+  @override
+  List<Object?> get props => [category];
+}
+
+/// Event to signal that the form should be submitted.
+class EditHeadlineSubmitted extends EditHeadlineEvent {
+  const EditHeadlineSubmitted();
+}
+
