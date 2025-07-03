b3f211884f1703f06ac6716888718ccf03a5c476
fulleni <fulleni@hotmail.com>
2025-07-03 08:30:12 +0100

    feat(bloc-observer): add print statements for onChange and onError logging

--- lib/bloc_observer.dart
+++ lib/bloc_observer.dart
@@ -9,11 +9,13 @@ class AppBlocObserver extends BlocObserver {
   void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
     super.onChange(bloc, change);
     log('onChange(${bloc.runtimeType}, $change)');
+    print('onChange(${bloc.runtimeType}, $change)');
   }
 
   @override
   void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
     log('onError(${bloc.runtimeType}, $error, $stackTrace)');
+    print('onError(${bloc.runtimeType}, $error, $stackTrace)');
     super.onError(bloc, error, stackTrace);
   }
 }
