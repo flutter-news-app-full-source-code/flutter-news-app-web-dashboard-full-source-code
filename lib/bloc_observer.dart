15fb076d049e83cdd8963b09e64a1e7164599585
fulleni <fulleni@hotmail.com>
2025-07-03 17:10:49 +0100

    refactor: remove unused import and comment out log statements in AppBlocObserver

--- lib/bloc_observer.dart
+++ lib/bloc_observer.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:bloc/bloc.dart';
 
 class AppBlocObserver extends BlocObserver {
@@ -8,13 +6,13 @@ class AppBlocObserver extends BlocObserver {
   @override
   void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
     super.onChange(bloc, change);
-    log('onChange(${bloc.runtimeType}, $change)');
+    // log('onChange(${bloc.runtimeType}, $change)');
     print('onChange(${bloc.runtimeType}, $change)');
   }
 
   @override
   void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
-    log('onError(${bloc.runtimeType}, $error, $stackTrace)');
+    // log('onError(${bloc.runtimeType}, $error, $stackTrace)');
     print('onError(${bloc.runtimeType}, $error, $stackTrace)');
     super.onError(bloc, error, stackTrace);
   }
