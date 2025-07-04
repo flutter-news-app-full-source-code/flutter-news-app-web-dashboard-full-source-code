4ba5817bd1b30e7f93108ddf18ac89e708f014a0
fulleni <fulleni@hotmail.com>
2025-07-04 06:00:42 +0100

    refactor(content): refresh headlines list after creation

--- lib/content_management/view/create_headline_page.dart
+++ lib/content_management/view/create_headline_page.dart
@@ -4,6 +4,7 @@ import 'package:go_router/go_router.dart';
 import 'package:ht_dashboard/content_management/bloc/content_management_bloc.dart';
 import 'package:ht_dashboard/content_management/bloc/create_headline/create_headline_bloc.dart';
 import 'package:ht_dashboard/l10n/l10n.dart';
+import 'package:ht_dashboard/shared/constants/pagination_constants.dart';
 import 'package:ht_dashboard/shared/shared.dart';
 import 'package:ht_data_repository/ht_data_repository.dart';
 import 'package:ht_shared/ht_shared.dart';
@@ -83,7 +84,8 @@ class _CreateHeadlineViewState extends State<_CreateHeadlineView> {
                 SnackBar(content: Text(l10n.headlineCreatedSuccessfully)),
               );
             context.read<ContentManagementBloc>().add(
-              HeadlineAdded(state.createdHeadline!),
+                  // Refresh the list to show the new headline
+                  const LoadHeadlinesRequested(limit: kDefaultRowsPerPage),
             );
             context.pop();
           }
