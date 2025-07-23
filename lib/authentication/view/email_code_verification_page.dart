c6164219e14b349ebdcfb930a9a7760b46da4cfb
fulleni <fulleni@hotmail.com>
2025-07-23 12:04:59 +0100

    refactor(authentication): update package imports to flutter_news_app_web_dashboard_full_source_code
    - Replace 'ht_dashboard' package imports with 'flutter_news_app_web_dashboard_full_source_code'
    - Update 'ht_ui_kit' to 'ui_kit'
    - Apply consistent package import structure across multiple files

--- lib/authentication/view/email_code_verification_page.dart
+++ lib/authentication/view/email_code_verification_page.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:ht_dashboard/app/bloc/app_bloc.dart';
-import 'package:ht_dashboard/app/config/config.dart';
-import 'package:ht_dashboard/authentication/bloc/authentication_bloc.dart';
-import 'package:ht_dashboard/l10n/l10n.dart';
-import 'package:ht_ui_kit/ht_ui_kit.dart';
+import 'package:flutter_news_app_web_dashboard_full_source_code/app/bloc/app_bloc.dart';
+import 'package:flutter_news_app_web_dashboard_full_source_code/app/config/config.dart';
+import 'package:flutter_news_app_web_dashboard_full_source_code/authentication/bloc/authentication_bloc.dart';
+import 'package:flutter_news_app_web_dashboard_full_source_code/l10n/l10n.dart';
+import 'package:ui_kit/ui_kit.dart';
 
 /// {@template email_code_verification_page}
 /// Page where the user enters the 6-digit code sent to their email
