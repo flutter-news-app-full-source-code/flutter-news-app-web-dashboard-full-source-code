eb20029d863aa09f709cc5d0c97459b25cd56dc1
fulleni <fulleni@hotmail.com>
2025-07-28 18:07:31 +0100

    refactor(authentication): remove unused import
    - Remove unused import statement for 'package:flutter/services.dart'
    - This change simplifies the code and reduces potential confusion for developers

--- lib/authentication/view/email_code_verification_page.dart
+++ lib/authentication/view/email_code_verification_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_news_app_web_dashboard_full_source_code/app/bloc/app_bloc.dart';
 import 'package:flutter_news_app_web_dashboard_full_source_code/app/config/config.dart';
