34bdf070b3943e9e8e67af05f333e02a9959dfda
fulleni <fulleni@hotmail.com>
2025-07-31 18:44:35 +0100

    refactor: remove unused SourceTypeL10n extension
    - Deleted the SourceTypeL10n extension from edit_source_bloc.dart
    - This extension was likely redundant, given that similar localization
      functionality is already provided by the existing SourceType extension
    - Removing this code improves maintainability and reduces potential
      confusion in the codebase

--- lib/content_management/bloc/edit_source/edit_source_bloc.dart
+++ lib/content_management/bloc/edit_source/edit_source_bloc.dart
@@ -218,33 +218,3 @@ class EditSourceBloc extends Bloc<EditSourceEvent, EditSourceState> {
     }
   }
 }
-
-/// Adds localization support to the [SourceType] enum.
-extension SourceTypeL10n on SourceType {
-  /// Returns the localized name for the source type.
-  ///
-  /// This requires an [AppLocalizations] instance, which is typically
-  /// retrieved from the build context.
-  String localizedName(AppLocalizations l10n) {
-    switch (this) {
-      case SourceType.newsAgency:
-        return l10n.sourceTypeNewsAgency;
-      case SourceType.localNewsOutlet:
-        return l10n.sourceTypeLocalNewsOutlet;
-      case SourceType.nationalNewsOutlet:
-        return l10n.sourceTypeNationalNewsOutlet;
-      case SourceType.internationalNewsOutlet:
-        return l10n.sourceTypeInternationalNewsOutlet;
-      case SourceType.specializedPublisher:
-        return l10n.sourceTypeSpecializedPublisher;
-      case SourceType.blog:
-        return l10n.sourceTypeBlog;
-      case SourceType.governmentSource:
-        return l10n.sourceTypeGovernmentSource;
-      case SourceType.aggregator:
-        return l10n.sourceTypeAggregator;
-      case SourceType.other:
-        return l10n.sourceTypeOther;
-    }
-  }
-}
