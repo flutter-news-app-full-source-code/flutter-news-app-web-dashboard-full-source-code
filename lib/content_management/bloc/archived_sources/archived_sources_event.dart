5059e8afc747d30989c977c3f215ff44850c0ee3
fulleni <fulleni@hotmail.com>
2025-08-02 12:25:31 +0100

    refactor(archived_sources): update props type in ArchivedSourcesEvent to support nullable types

--- lib/content_management/bloc/archived_sources/archived_sources_event.dart
+++ lib/content_management/bloc/archived_sources/archived_sources_event.dart
@@ -4,5 +4,26 @@ sealed class ArchivedSourcesEvent extends Equatable {
   const ArchivedSourcesEvent();
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
+}
+
+/// Event to request loading of archived sources.
+final class LoadArchivedSourcesRequested extends ArchivedSourcesEvent {
+  const LoadArchivedSourcesRequested({this.startAfterId, this.limit});
+
+  final String? startAfterId;
+  final int? limit;
+
+  @override
+  List<Object?> get props => [startAfterId, limit];
+}
+
+/// Event to restore an archived source.
+final class RestoreSourceRequested extends ArchivedSourcesEvent {
+  const RestoreSourceRequested(this.id);
+
+  final String id;
+
+  @override
+  List<Object?> get props => [id];
 }
