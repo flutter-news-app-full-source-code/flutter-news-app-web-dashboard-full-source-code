a65be922b4a8672b709bf78980b01d27f74d7057
fulleni <fulleni@hotmail.com>
2025-08-02 18:26:13 +0100

    feat: add UndoDeleteHeadlineRequested and _ConfirmDeleteHeadlineRequested events for headline management

--- lib/content_management/bloc/archived_headlines/archived_headlines_event.dart
+++ lib/content_management/bloc/archived_headlines/archived_headlines_event.dart
@@ -37,3 +37,18 @@ final class DeleteHeadlineForeverRequested extends ArchivedHeadlinesEvent {
   @override
   List<Object?> get props => [id];
 }
+
+/// Event to undo the deletion of a headline.
+final class UndoDeleteHeadlineRequested extends ArchivedHeadlinesEvent {
+  const UndoDeleteHeadlineRequested();
+}
+
+/// Internal event to confirm the permanent deletion of a headline after a delay.
+final class _ConfirmDeleteHeadlineRequested extends ArchivedHeadlinesEvent {
+  const _ConfirmDeleteHeadlineRequested(this.id);
+
+  final String id;
+
+  @override
+  List<Object?> get props => [id];
+}
