a67b062b47234d0dc953908ab6fd4ea1c63f12a8
fulleni <fulleni@hotmail.com>
2025-08-02 12:11:36 +0100

    refactor(content_management): update props type in ArchivedTopicsEvent to support nullable types

--- lib/content_management/bloc/archived_topics/archived_topics_event.dart
+++ lib/content_management/bloc/archived_topics/archived_topics_event.dart
@@ -4,5 +4,26 @@ sealed class ArchivedTopicsEvent extends Equatable {
   const ArchivedTopicsEvent();
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
+}
+
+/// Event to request loading of archived topics.
+final class LoadArchivedTopicsRequested extends ArchivedTopicsEvent {
+  const LoadArchivedTopicsRequested({this.startAfterId, this.limit});
+
+  final String? startAfterId;
+  final int? limit;
+
+  @override
+  List<Object?> get props => [startAfterId, limit];
+}
+
+/// Event to restore an archived topic.
+final class RestoreTopicRequested extends ArchivedTopicsEvent {
+  const RestoreTopicRequested(this.id);
+
+  final String id;
+
+  @override
+  List<Object?> get props => [id];
 }
