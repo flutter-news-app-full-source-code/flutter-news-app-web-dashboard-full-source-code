b13d6f0c6caba8e82358cf77e8c942a0c84af5a3
fulleni <fulleni@hotmail.com>
2025-09-01 08:44:06 +0100

    feat(local_ads_management): add update local ads bloc
    - Create UpdateLocalAdsBloc class extending Bloc
    - Define UpdateLocalAdsEvent sealed class
    - Define UpdateLocalAdsState sealed class with initial state

---
+++ lib/local_ads_management/bloc/update_local_ads/update_local_ads_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'update_local_ads_event.dart';
+part 'update_local_ads_state.dart';
+
+class UpdateLocalAdsBloc extends Bloc<UpdateLocalAdsEvent, UpdateLocalAdsState> {
+  UpdateLocalAdsBloc() : super(UpdateLocalAdsInitial()) {
+    on<UpdateLocalAdsEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
