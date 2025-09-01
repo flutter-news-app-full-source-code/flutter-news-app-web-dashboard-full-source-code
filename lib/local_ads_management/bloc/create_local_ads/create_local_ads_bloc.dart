0b5908a1ba13a5ff81e3150bd9cb8d98ba0ca0b5
fulleni <fulleni@hotmail.com>
2025-09-01 08:43:56 +0100

    feat(local_ads_management): add create local ads bloc
    - Create CreateLocalAdsBloc class extending Bloc
    - Define CreateLocalAdsEvent sealed class
    - Define CreateLocalAdsState sealed class with initial state

---
+++ lib/local_ads_management/bloc/create_local_ads/create_local_ads_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'create_local_ads_event.dart';
+part 'create_local_ads_state.dart';
+
+class CreateLocalAdsBloc extends Bloc<CreateLocalAdsEvent, CreateLocalAdsState> {
+  CreateLocalAdsBloc() : super(CreateLocalAdsInitial()) {
+    on<CreateLocalAdsEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
---
+++ lib/local_ads_management/bloc/create_local_ads/create_local_ads_event.dart
@@ -0,0 +1,8 @@
+part of 'create_local_ads_bloc.dart';
+
+sealed class CreateLocalAdsEvent extends Equatable {
+  const CreateLocalAdsEvent();
+
+  @override
+  List<Object> get props => [];
+}
