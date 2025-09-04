37a56459314f8edc2673e0734935c6f1f81b3486
fulleni <fulleni@hotmail.com>
2025-09-04 12:25:24 +0100

    feat(extensions): add new localization exports for ad type and local ads management
    - Add 'ad_type_l10n.dart' and 'local_ads_management_tab_l10n.dart' to extensions

--- lib/shared/extensions/extensions.dart
+++ lib/shared/extensions/extensions.dart
@@ -1,8 +1,10 @@
 export 'ad_platform_type_l10n.dart';
+export 'ad_type_l10n.dart';
 export 'app_user_role_l10n.dart';
 export 'content_status_l10n.dart';
 export 'feed_decorator_type_l10n.dart';
 export 'in_article_ad_slot_type_l10n.dart';
 export 'local_ad_to_ad_type.dart';
+export 'local_ads_management_tab_l10n.dart';
 export 'source_type_l10n.dart';
 export 'string_truncate.dart';
