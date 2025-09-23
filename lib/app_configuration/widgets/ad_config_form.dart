eabfc06606acf3f423bfb8579fb3dd29c5eba102
fulleni <fulleni@hotmail.com>
2025-09-23 13:22:33 +0100

    refactor(ad_config_form): simplify AdConfigForm widget
    - Remove TabBar and role-based conditional rendering
    - Update widget documentation
    - Simplify state management by removing controllers and related methods

--- lib/app_configuration/widgets/ad_config_form.dart
+++ lib/app_configuration/widgets/ad_config_form.dart
@@ -3,10 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_news_app_web_dashboard_full_source_code/l10n/l10n.dart';
 
 /// {@template ad_config_form}
-/// A form widget for configuring ad settings based on user role.
+/// A form widget for configuring global ad settings.
 ///
-/// This widget uses a [TabBar] to allow selection of an [AppUserRole]
-/// and then conditionally renders the relevant input fields for that role.
+/// This widget primarily controls the global enable/disable switch for ads.
 /// {@endtemplate}
 class AdConfigForm extends StatefulWidget {
   /// {@macro ad_config_form}
@@ -26,96 +25,7 @@ class AdConfigForm extends StatefulWidget {
   State<AdConfigForm> createState() => _AdConfigFormState();
 }
 
-class _AdConfigFormState extends State<AdConfigForm>
-    with SingleTickerProviderStateMixin {
-  late TabController _tabController;
-  late final Map<AppUserRole, TextEditingController> _adFrequencyControllers;
-  late final Map<AppUserRole, TextEditingController>
-  _adPlacementIntervalControllers;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(
-      length: AppUserRole.values.length,
-      vsync: this,
-    );
-    _initializeControllers();
-  }
-
-  @override
-  void didUpdateWidget(covariant AdConfigForm oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (widget.remoteConfig.adConfig != oldWidget.remoteConfig.adConfig) {
-      _updateControllers();
-    }
-  }
-
-  void _initializeControllers() {
-    final adConfig = widget.remoteConfig.adConfig;
-    _adFrequencyControllers = {
-      for (final role in AppUserRole.values)
-        role:
-            TextEditingController(
-                text: _getAdFrequency(adConfig, role).toString(),
-              )
-              ..selection = TextSelection.collapsed(
-                offset: _getAdFrequency(adConfig, role).toString().length,
-              ),
-    };
-    _adPlacementIntervalControllers = {
-      for (final role in AppUserRole.values)
-        role:
-            TextEditingController(
-                text: _getAdPlacementInterval(adConfig, role).toString(),
-              )
-              ..selection = TextSelection.collapsed(
-                offset: _getAdPlacementInterval(
-                  adConfig,
-                  role,
-                ).toString().length,
-              ),
-    };
-  }
-
-  void _updateControllers() {
-    final adConfig = widget.remoteConfig.adConfig;
-    for (final role in AppUserRole.values) {
-      final newFrequencyValue = _getAdFrequency(adConfig, role).toString();
-      if (_adFrequencyControllers[role]?.text != newFrequencyValue) {
-        _adFrequencyControllers[role]?.text = newFrequencyValue;
-        _adFrequencyControllers[role]?.selection = TextSelection.collapsed(
-          offset: newFrequencyValue.length,
-        );
-      }
-
-      final newPlacementIntervalValue = _getAdPlacementInterval(
-        adConfig,
-        role,
-      ).toString();
-      if (_adPlacementIntervalControllers[role]?.text !=
-          newPlacementIntervalValue) {
-        _adPlacementIntervalControllers[role]?.text = newPlacementIntervalValue;
-        _adPlacementIntervalControllers[role]?.selection =
-            TextSelection.collapsed(
-              offset: newPlacementIntervalValue.length,
-            );
-      }
-    }
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    for (final controller in _adFrequencyControllers.values) {
-      controller.dispose();
-    }
-    for (final controller in _adPlacementIntervalControllers.values) {
-      controller.dispose();
-    }
-    super.dispose();
-  }
-
+class _AdConfigFormState extends State<AdConfigForm> {
   @override
   Widget build(BuildContext context) {
     final adConfig = widget.remoteConfig.adConfig;
@@ -137,39 +47,5 @@ class _AdConfigFormState extends State<AdConfigForm>
         ),
       ],
     );
-  }
-
-  int _getAdFrequency(AdConfig config, AppUserRole role) {
-    switch (role) {
-      case AppUserRole.guestUser:
-        return config.feedAdConfiguration.frequencyConfig.guestAdFrequency;
-      case AppUserRole.standardUser:
-        return config
-            .feedAdConfiguration
-            .frequencyConfig
-            .authenticatedAdFrequency;
-      case AppUserRole.premiumUser:
-        return config.feedAdConfiguration.frequencyConfig.premiumAdFrequency;
-    }
-  }
-
-  int _getAdPlacementInterval(AdConfig config, AppUserRole role) {
-    switch (role) {
-      case AppUserRole.guestUser:
-        return config
-            .feedAdConfiguration
-            .frequencyConfig
-            .guestAdPlacementInterval;
-      case AppUserRole.standardUser:
-        return config
-            .feedAdConfiguration
-            .frequencyConfig
-            .authenticatedAdPlacementInterval;
-      case AppUserRole.premiumUser:
-        return config
-            .feedAdConfiguration
-            .frequencyConfig
-            .premiumAdPlacementInterval;
-    }
   }
 }
