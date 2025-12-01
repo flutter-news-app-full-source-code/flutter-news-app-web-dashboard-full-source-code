0f374d485a4919150fa90a34e4ea2d17f1bc01ac
fulleni <fulleni@hotmail.com>
2025-12-01 08:53:34 +0100

    feat(community_management): add CommunityFilterState class
    - Create a new CommunityFilterState class for managing community filter states
    - Include properties for search query, comment status, report status, reportable entity, and initial feedback
    - Implement copyWith method for state immutability
    - Override props for Equatable comparison

---
+++ lib/community_management/bloc/community_filter/community_filter_state.dart
@@ -0,0 +1,45 @@
+part of 'community_filter_bloc.dart';
+
+class CommunityFilterState extends Equatable {
+  const CommunityFilterState({
+    this.searchQuery = '',
+    this.selectedCommentStatus = const [],
+    this.selectedReportStatus = const [],
+    this.selectedReportableEntity = const [],
+    this.selectedInitialFeedback = const [],
+  });
+
+  final String searchQuery;
+  final List<CommentStatus> selectedCommentStatus;
+  final List<ReportStatus> selectedReportStatus;
+  final List<ReportableEntity> selectedReportableEntity;
+  final List<InitialAppReviewFeedback> selectedInitialFeedback;
+
+  CommunityFilterState copyWith({
+    String? searchQuery,
+    List<CommentStatus>? selectedCommentStatus,
+    List<ReportStatus>? selectedReportStatus,
+    List<ReportableEntity>? selectedReportableEntity,
+    List<InitialAppReviewFeedback>? selectedInitialFeedback,
+  }) {
+    return CommunityFilterState(
+      searchQuery: searchQuery ?? this.searchQuery,
+      selectedCommentStatus:
+          selectedCommentStatus ?? this.selectedCommentStatus,
+      selectedReportStatus: selectedReportStatus ?? this.selectedReportStatus,
+      selectedReportableEntity:
+          selectedReportableEntity ?? this.selectedReportableEntity,
+      selectedInitialFeedback:
+          selectedInitialFeedback ?? this.selectedInitialFeedback,
+    );
+  }
+
+  @override
+  List<Object> get props => [
+    searchQuery,
+    selectedCommentStatus,
+    selectedReportStatus,
+    selectedReportableEntity,
+    selectedInitialFeedback,
+  ];
+}
