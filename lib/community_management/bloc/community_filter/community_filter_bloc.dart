ddee43f239c40c479ca485035ab63cc9e72cf935
fulleni <fulleni@hotmail.com>
2025-12-01 08:47:35 +0100

    feat(community_management): implement CommunityFilterBloc
    - Create CommunityFilterBloc to manage community filter state
    - Add event handlers for filter application and reset
    - Define state properties for search query

---
+++ lib/community_management/bloc/community_filter/community_filter_bloc.dart
@@ -0,0 +1,38 @@
+import 'package:bloc/bloc.dart';
+import 'package:core/core.dart';
+import 'package:equatable/equatable.dart';
+
+part 'community_filter_event.dart';
+part 'community_filter_state.dart';
+
+class CommunityFilterBloc
+    extends Bloc<CommunityFilterEvent, CommunityFilterState> {
+  CommunityFilterBloc() : super(const CommunityFilterState()) {
+    on<CommunityFilterApplied>(_onFilterApplied);
+    on<CommunityFilterReset>(_onFilterReset);
+  }
+
+  void _onFilterApplied(
+    CommunityFilterApplied event,
+    Emitter<CommunityFilterState> emit,
+  ) {
+    emit(
+      state.copyWith(
+        searchQuery: event.searchQuery,
+        selectedCommentStatus: event.selectedCommentStatus,
+        selectedReportStatus: event.selectedReportStatus,
+        selectedReportableEntity: event.selectedReportableEntity,
+        selectedInitialFeedback: event.selectedInitialFeedback,
+      ),
+    );
+  }
+
+  void _onFilterReset(
+    CommunityFilterReset event,
+    Emitter<CommunityFilterState> emit,
+  ) {
+    emit(
+      const CommunityFilterState(),
+    );
+  }
+}
