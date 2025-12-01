a2d32102f303f46d1d75201d2e102c4f6328f749
fulleni <fulleni@hotmail.com>
2025-12-01 12:04:37 +0100

    feat(l10n): add ReportableEntity localization extension
        Introduces a localization extension for ReportableEntity enum to provide
        admin-centric display strings for types of reported content.

---
+++ lib/shared/extensions/reportable_entity_extension.dart
@@ -0,0 +1,18 @@
+import 'package:core/core.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_news_app_web_dashboard_full_source_code/l10n/l10n.dart';
+
+extension ReportableEntityX on ReportableEntity {
+  /// Returns a localized, admin-centric string for the [ReportableEntity].
+  String l10n(BuildContext context) {
+    final l10n = context.l10n;
+    switch (this) {
+      case ReportableEntity.headline:
+        return l10n.reportableEntityHeadline;
+      case ReportableEntity.source:
+        return l10n.reportableEntitySource;
+      case ReportableEntity.comment:
+        return l10n.reportableEntityComment;
+    }
+  }
+}
