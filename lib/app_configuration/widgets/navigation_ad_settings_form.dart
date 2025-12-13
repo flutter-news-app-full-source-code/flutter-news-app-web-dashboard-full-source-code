33a3ffaffe600b18fa63152a86b651a89302fb51
fulleni <fulleni@hotmail.com>
2025-12-13 14:24:47 +0100

    feat(app_configuration): add descriptions to navigation ad settings switches
    - Add subtitle to enableNavigationAdsLabel switch
    - Add subtitle to visibleToRoleLabel switch

--- lib/app_configuration/widgets/navigation_ad_settings_form.dart
+++ lib/app_configuration/widgets/navigation_ad_settings_form.dart
@@ -130,6 +130,7 @@ class _NavigationAdSettingsFormState extends State<NavigationAdSettingsForm>
       children: [
         SwitchListTile(
           title: Text(l10n.enableNavigationAdsLabel),
+          subtitle: Text(l10n.enableNavigationAdsDescription),
           value: navAdConfig.enabled,
           onChanged: (value) {
             widget.onConfigChanged(
@@ -212,6 +213,7 @@ class _NavigationAdSettingsFormState extends State<NavigationAdSettingsForm>
         children: [
           SwitchListTile(
             title: Text(l10n.visibleToRoleLabel(role.l10n(context))),
+            subtitle: Text(l10n.visibleToRoleDescription(role.l10n(context))),
             value: roleConfig != null,
             onChanged: (value) {
               final newVisibleTo =
