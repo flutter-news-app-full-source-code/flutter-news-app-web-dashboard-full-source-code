caab043480c2b8e0e10f686573cc85251a559a2f
fulleni <fulleni@hotmail.com>
2025-12-21 05:03:49 +0100

    chore: barrels

---
+++ lib/shared/widgets/analytics/analytics.dart
@@ -0,0 +1,5 @@
+export 'analytics_card_shell.dart';
+export 'analytics_card_slot.dart';
+export 'chart_card.dart';
+export 'kpi_card.dart';
+export 'ranked_list_card.dart';
--- lib/shared/widgets/widgets.dart
+++ lib/shared/widgets/widgets.dart
@@ -1,4 +1,5 @@
 export 'about_icon.dart';
+export 'analytics/analytics.dart';
 export 'confirmation_dialog.dart';
 export 'searchable_selection_input.dart';
 export 'selection_page/selection_page.dart';
