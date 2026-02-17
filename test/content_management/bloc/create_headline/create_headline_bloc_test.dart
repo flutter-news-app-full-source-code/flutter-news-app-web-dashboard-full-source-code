8e83b9523ed96ffcbcee3a0e7bbaee403dad79bb
Fulleni Flen <fulleni@hotmail.com>
2026-02-17 17:31:29 +0100

    style: format

--- test/content_management/bloc/create_headline/create_headline_bloc_test.dart
+++ test/content_management/bloc/create_headline/create_headline_bloc_test.dart
@@ -2,9 +2,7 @@ import 'dart:typed_data';
 
 import 'package:core/core.dart';
 import 'package:flutter_news_app_web_dashboard_full_source_code/content_management/bloc/create_headline/create_headline_bloc.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:logging/logging.dart';
-import 'package:mocktail/mocktail.dart';
 
 import '../../../helpers/helpers.dart';
 
