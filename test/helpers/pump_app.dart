0451744ad170a77c82f82763fbd9f78a0a84d430
Fulleni Flen <fulleni@hotmail.com>
2026-02-18 07:03:33 +0100

    test: update pumpApp extension for localization and routing
    - Add ui_kit localization delegate
    - Use aliased import for go_router package
    - Update GoRouter usage to match new package alias
    - Add null safety check for goRouter parameter

--- test/helpers/pump_app.dart
+++ test/helpers/pump_app.dart
@@ -1,24 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_news_app_web_dashboard_full_source_code/l10n/app_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:go_router/go_router.dart';
-import 'package:mocktail/mocktail.dart';
-
-class MockGoRouter extends Mock implements GoRouter {}
+import 'package:go_router/go_router.dart' as go_router;
+import 'package:ui_kit/ui_kit.dart';
 
 extension PumpApp on WidgetTester {
   Future<void> pumpApp(
     Widget widget, {
-    GoRouter? goRouter,
+    go_router.GoRouter? goRouter,
   }) {
     return pumpWidget(
       MaterialApp(
-        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        localizationsDelegates: const [
+          ...AppLocalizations.localizationsDelegates,
+          UiKitLocalizations.delegate,
+        ],
         supportedLocales: AppLocalizations.supportedLocales,
         home: goRouter == null
             ? widget
-            : InheritedGoRouter(
-                goRouter: goRouter,
+            : go_router.InheritedGoRouter(
+                goRouter: goRouter!,
                 child: widget,
               ),
       ),
