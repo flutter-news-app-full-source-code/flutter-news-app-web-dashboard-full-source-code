7a1159b31105be90d6d2d2522a41803f9ba86f62
Fulleni Flen <fulleni@hotmail.com>
2026-02-23 11:24:11 +0100

    refactor(app_configuration): replace UserConfigurationTab with LimitsConfigurationTab
    - Remove import of user_configuration_tab.dart
    - Add import of limits_configuration_tab.dart
    - Replace Tab text from l10n.userTab to l10n.limitsTab
    - Replace UserConfigurationTab with LimitsConfigurationTab in tab content

--- lib/app_configuration/view/app_configuration_page.dart
+++ lib/app_configuration/view/app_configuration_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_news_app_web_dashboard_full_source_code/app_configuration/bloc/app_configuration_bloc.dart';
 import 'package:flutter_news_app_web_dashboard_full_source_code/app_configuration/view/tabs/features_configuration_tab.dart';
+import 'package:flutter_news_app_web_dashboard_full_source_code/app_configuration/view/tabs/limits_configuration_tab.dart';
 import 'package:flutter_news_app_web_dashboard_full_source_code/app_configuration/view/tabs/system_configuration_tab.dart';
-import 'package:flutter_news_app_web_dashboard_full_source_code/app_configuration/view/tabs/user_configuration_tab.dart';
 import 'package:flutter_news_app_web_dashboard_full_source_code/l10n/l10n.dart';
 import 'package:flutter_news_app_web_dashboard_full_source_code/shared/widgets/about_icon.dart';
 import 'package:ui_kit/ui_kit.dart';
@@ -67,7 +67,7 @@ class _AppConfigurationPageState extends State<AppConfigurationPage>
           tabs: [
             Tab(text: l10n.systemTab),
             Tab(text: l10n.featuresTab),
-            Tab(text: l10n.userTab),
+            Tab(text: l10n.limitsTab),
           ],
         ),
       ),
@@ -148,7 +148,7 @@ class _AppConfigurationPageState extends State<AppConfigurationPage>
                     );
                   },
                 ),
-                UserConfigurationTab(
+                LimitsConfigurationTab(
                   remoteConfig: remoteConfig,
                   onConfigChanged: (newConfig) {
                     context.read<AppConfigurationBloc>().add(
